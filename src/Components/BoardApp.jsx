79f6c3dc94aaa336f5f9056c541d87b66ce41886
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-14 21:11:00 +0900

    파이어베이스 추가 및 Config작성

--- src/Components/BoardApp.jsx
+++ src/Components/BoardApp.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Firebase from "../Firebase/Firebase";
 
 function BoardApp() {
+    const [post, setPost] = useState([]);
+    console.log(post)
+
     return (
         <div>
             <h2>This is BoardApp</h2>
         </div>
-    )
+    );
 }
 
-export default BoardApp
+export default BoardApp;
---
+++ src/Firebase/Firebase.jsx
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const fire_Config = {
+    apiKey: "AIzaSyAY5OOI-G4fD48Lbb0bbGxftI88qQa1P2A",
+    authDomain: "project20200108.firebaseapp.com",
+    databaseURL: "https://project20200108.firebaseio.com",
+    projectId: "project20200108",
+    storageBucket: "project20200108.appspot.com",
+    messagingSenderId: "68945753268",
+    appId: "1:68945753268:web:41cc5f13b0309cfe5bd3e1",
+    measurementId: "G-63ZTSX6FP4"
+};
+// Initialize Firebase
+firebase.initializeApp(fire_Config);
+
+
