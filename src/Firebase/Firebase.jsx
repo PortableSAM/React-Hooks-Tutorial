10a202e6435bd51a3484695cd16288214c575712
PortableSAM <53569830+PortableSAM@users.noreply.github.com>
2020-01-16 23:32:05 +0900

    Update Firebase.jsx

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -4,14 +4,14 @@
 // Your web app's Firebase configuration
 //{
 const fire_Config = {
-  apiKey: "AIzaSyAY5OOI-G4fD48Lbb0bbGxftI88qQa1P2A",
-  authDomain: "project20200108.firebaseapp.com",
-  databaseURL: "https://project20200108.firebaseio.com",
-  projectId: "project20200108",
-  storageBucket: "project20200108.appspot.com",
-  messagingSenderId: "68945753268",
-  appId: "1:68945753268:web:41cc5f13b0309cfe5bd3e1",
-  measurementId: "G-63ZTSX6FP4"
+  apiKey: "###",
+  authDomain: "###",
+  databaseURL: "###",
+  projectId: "###",
+  storageBucket: "###",
+  messagingSenderId: "###",
+  appId: "###",
+  measurementId: "###""
 };
 
 //const firestore = firebase.firestore();
